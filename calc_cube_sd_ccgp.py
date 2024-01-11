8fc1933c31d05490795f3bafe9d8e0ac74fa3dca
Benjie Miao <bjmiao501@gmail.com>
2024-01-11 22:43:02 +0800

    update slurm script and upload result

--- calc_cube_sd_ccgp.py
+++ calc_cube_sd_ccgp.py
@@ -5,40 +5,30 @@ import pandas as pd
 import random
 import string
 
-import logging
-# Configure the logging settings
-logging.basicConfig(filename='results/nsample_effect.log', level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-
-
 if __name__ == "__main__":
-    cube_dimension = 3
+    cube_dimension = 4
     embedding_dimension = int(sys.argv[1])
     noise_coef = float(sys.argv[2])
     distortion_magnitude = float(sys.argv[3])
 
     a1, a2 = predefine_dimension_for_ccgp(cube_dimension)
-    print(a1, a2)
+    # print(a1, a2)
     # a1 = [[0,1,2,3,4,5,6,7], [0,1,4,5,8,9,12,13], [0,2,4,6,8,10,12,14], [0,1,2,3,8,9,10,11]]
     # a2 = [[x for x in range(16) if x not in a1_this] for a1_this in a1]
     # for embedding_dimension in [10, 30, 50, 80, 100, 200, 300, 400, 500]:
-    for embedding_dimension in [8, 15, 80, 100, 200, 300, 400, 500]:
-        cube = generate_hypercube_in_embedding_space(cube_dimension, embedding_dimension, distortion_magnitude=distortion_magnitude)
-        for nsamples in [10, 20, 30, 50, 80, 100, 200, 300, 500, 800, 1000]:
-            sd = shattering_dimensionality(
-                cube,
-                nsamples = nsamples, noise_coef = noise_coef,
-            )
-            ccgp = CCGP(
-                cube,
-                nsamples = nsamples,
-                noise_coef = noise_coef,
-                predefined_dichotomy=(a1, a2)
-            )
-            print(cube_dimension, nsamples, embedding_dimension, sd, ccgp)
-            logging.info(f"{cube_dimension},{embedding_dimension},{distortion_magnitude},{noise_coef},{nsamples},{sd},{ccgp}")
-
-    # res = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
-    # with open("result_"+res+".txt", "w") as f:
-    #     f.write(f"{cube_dimension},{embedding_dimension},{noise_coef},{distortion_magnitude},{sd},{ccgp}\n")
-
+    nsamples = 1000
+    nsample_dichotomy = 100
+    cube = generate_hypercube_in_embedding_space(cube_dimension, embedding_dimension, distortion_magnitude=distortion_magnitude)
+    sd = shattering_dimensionality(
+        cube,
+        nsamples = nsamples, noise_coef = noise_coef,
+        sample_dichotomy = nsample_dichotomy
+    )
+    ccgp = CCGP(
+        cube,
+        nsamples = nsamples,
+        noise_coef = noise_coef,
+        predefined_dichotomy=(a1, a2)
+    )
+    print("cube_sd_head", cube_dimension, embedding_dimension, noise_coef, distortion_magnitude, sd, ccgp, flush=True)
+    #logging.info(f"{cube_dimension},{embedding_dimension},{distortion_magnitude},{noise_coef},{nsamples},{sd},{ccgp}")
--- slurm/runall.py
+++ slurm/runall.py
@@ -3,11 +3,12 @@ import subprocess
 import numpy as np
 
 filename = "batch.sbatch" 
+executive_name = "../calc_cube_sd_ccgp.py"
 
-for embedding_dimension in range(40, 400, 20):
-    for noise_coef in [0.2, 1]:
-        for distortion_magnitude in np.arange(0, 4, 0.25):
-            p = subprocess.Popen(["sbatch", filename, str(embedding_dimension), str(noise_coef), str(distortion_magnitude)], stdout=subprocess.PIPE, 
+for embedding_dimension in range(40, 401, 20):
+    for noise_coef in [0, 0.2, 1]:
+        for distortion_magnitude in np.arange(0, 4.01, 0.25):
+            p = subprocess.Popen(["sbatch", filename, executive_name, str(embedding_dimension), str(noise_coef), str(distortion_magnitude)], stdout=subprocess.PIPE, 
                                         stderr=subprocess.PIPE)
             out, err = p.communicate()
             print(out.decode(), err.decode())
